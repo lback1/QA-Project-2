0881a45dfc63c3b55ff4e1d552227e193bb74a04
Luke Back <luke.back@hotmail.com>
2022-07-05 08:43:49 +0000

    update

--- Service2/application/routes.py
+++
@@ -1,7 +0,0 @@
-from application import app
-import random
-
-@app.route('/weapons', methods=['GET'])
-def weapons():
-    weapons = ['Heavy Sniper', 'SCAR', 'Drum Gun', 'Compact SMG', 'Tactical Shotgun', 'Dual Pistols', 'Rocket Launcher', 'Grenade Launcher']
-    return random.choice(weapons)
--- Service3/application/routes.py
+++
@@ -1,7 +0,0 @@
-from application import app
-import random
-
-@app.route('/rarities', methods=['GET'])
-def rarities():
-    rarities = ['Common', 'Uncommon', 'Rare', 'Epic', 'Legendary', 'Mythic']
-    return random.choice(rarities)
